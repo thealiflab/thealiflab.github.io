05e5ff1c9eca3e92dae76b4085495038ad45de93
Alif <ahmedalif1010@gmail.com>
2024-10-08 20:10:08 +1100

    fix the bug for DOMContentLoaded error

--- assets/myjs/submitform.js
+++ assets/myjs/submitform.js
@@ -1,30 +1,33 @@
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+document.addEventListener('DOMContentLoaded', function() {
 
-    var form = this;
-    var data = new FormData(form);
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
 
-    fetch(form.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            form.reset(); // Clear the form
-            document.getElementById('resultMessage').style.display = 'block'; // Show the success message
-        } else {
-            response.json().then(data => {
-                if (data.errors) {
-                    alert('Something went wrong. Please check your entry and try again.');
-                } else {
-                    alert('Oops! There was a problem submitting your form');
-                }
-            });
-        }
-    }).catch(error => {
-        alert('Error: ' + error);
+        var form = this;
+        var data = new FormData(form);
+
+        fetch(form.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                form.reset(); // Clear the form
+                document.getElementById('resultMessage').style.display = 'block'; // Show the success message
+            } else {
+                response.json().then(data => {
+                    if (data.errors) {
+                        alert('Something went wrong. Please check your entry and try again.');
+                    } else {
+                        alert('Oops! There was a problem submitting your form');
+                    }
+                });
+            }
+        }).catch(error => {
+            alert('Error: ' + error);
+        });
     });
 });
